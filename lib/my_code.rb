b5498d97270f98d25beb2eef76beff16bb689c8b
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-22 19:01:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,4 +55,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i]
+      return TRUE
+    end
+    i += 1
+  end
+  FALSE
 end
