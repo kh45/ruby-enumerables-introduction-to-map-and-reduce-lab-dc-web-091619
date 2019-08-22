9e477e4c639ac1b7624ce40ad0df82f4cc659482
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-22 17:35:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 # My Code here....
 def map_to_negativize(source_array)
-  
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array[i] = source_array[i] * -1
+    i += 1
+  end
+  new_array
 end
 
 def map_to_no_change(source_array)
